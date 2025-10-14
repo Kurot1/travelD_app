c4cc84bc56d4821a4adf11837932a9063c8680e1
Kurot1 <kurotsuno365@gmail.com>
2025-10-14 17:00:22 +0900

    11

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
@@ -7,12 +8,14 @@ import 'pages/plan_form_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   await FlutterNaverMap().init(
-    clientId: 'b3tp7muoxf', // ← 너의 Client ID
+    clientId: dotenv.env['NAVER_CLIENT_ID']!,  // ← env에서 불러오기
     onAuthFailed: (ex) => print('NaverMap 인증 실패: $ex'),
   );
 
